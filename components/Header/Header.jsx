6193997318794cc01fd38c5d9b5c768309d980f5
NickolasCost <nikolascosty@gmail.com>
2019-09-07 12:02:41 +0300

    Настройка структуры

---
+++ components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={'Styles.content'}>
+            
+        </div>
+    );
+};
