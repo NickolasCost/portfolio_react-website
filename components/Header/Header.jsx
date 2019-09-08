ac1f1c4b74c00c13f6627764ca2d490cd045e6c8
NickolasCost <nikolascosty@gmail.com>
2019-09-08 09:50:24 +0300

    Изменения в позиционировании

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 const Header = () => {
     return (
         <div className={styles.content}>
-            <img src='https://cdn4.iconfinder.com/data/icons/logos-3/256/laravel-512.png' />
+            <img src='https://cdn4.iconfinder.com/data/icons/logos-3/256/laravel-512.png' alt='logo' />
         </div>
     );
 };
--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -4,9 +4,16 @@ import styles from './Profile.module.css';
 
 const Profile = () => {
     return (
+        <div>
         <div className={styles.container}>
             <img src="https://i.pinimg.com/originals/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64.jpg" alt="avatar"/>
+            <p className={styles.description}>Description</p>
         </div>
+        <MyPosts />
+        <MyPosts />
+        <MyPosts />
+        </div>
+
     )
 }
 
